1589330ae8389c9bdcd4706920fa83c74e070cc1
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-08 11:49:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,10 @@
 def speak_to_grandma(phrase)
   if phrase !== phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  end
   elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
+end
   elsif phrase == " I LOVE YOU, GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 end
